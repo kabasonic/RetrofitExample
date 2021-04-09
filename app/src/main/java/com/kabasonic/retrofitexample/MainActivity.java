36d5736fabc6ed9ccbb75f0e5545f4b5461d6888
kabasonic <foxymaan@gmail.com>
2021-04-09 12:28:06 +0200

    GET

--- app/src/main/java/com/kabasonic/retrofitexample/MainActivity.java
+++ app/src/main/java/com/kabasonic/retrofitexample/MainActivity.java
@@ -3,6 +3,15 @@ package com.kabasonic.retrofitexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        JsonApiPlaceHolder jsonApiPlaceHolder = retrofit.create(JsonApiPlaceHolder.class);
+
+        Call<List<Post>> postsList = jsonApiPlaceHolder.getPosts();
+
+        postsList.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                if(!response.isSuccessful()){
+                    Log.d(getClass().getSimpleName(),"onResponse|postList|Message" + response.message());
+                    return;
+                }
+                List<Post> callPosts = response.body();
+                for(Post post:callPosts){
+                    Log.i("#","##########");
+                    Log.i("#","--User ID: " + post.getUserId());
+                    Log.i("#","--ID: " + post.getId());
+                    Log.i("#","--Title: " + post.getTitle());
+                    Log.i("#","--Body: " + post.getText());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                Log.d(getClass().getSimpleName(),"onFailure|postsList|Message: " + t.getMessage());
+            }
+        });
+
     }
 }
