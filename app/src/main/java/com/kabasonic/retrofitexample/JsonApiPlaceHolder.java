9ebbb3c904e53a56dd0c6aff3c064efe3316d60c
kabasonic <foxymaan@gmail.com>
2021-04-09 16:08:56 +0200

    GET

--- app/src/main/java/com/kabasonic/retrofitexample/JsonApiPlaceHolder.java
+++ app/src/main/java/com/kabasonic/retrofitexample/JsonApiPlaceHolder.java
@@ -1,12 +1,38 @@
 package com.kabasonic.retrofitexample;
 
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
+import retrofit2.http.Url;
 
 public interface JsonApiPlaceHolder {
     @GET("posts")
     Call<List<Post>> getPosts();
 
+    @GET("posts/{id}/comments")
+    Call<List<Comment>> getCommentsForPost(@Path("id") int postId);
+
+    @GET("posts")
+    Call<List<Post>> getPostById(@Query("id") int postId);
+
+    @GET("posts")
+    Call<List<Post>> getPostByUserId(@Query("userId") int userId,
+                                     @Query("_sort") String sort,
+                                     @Query("_desc") String order);
+
+    @GET("posts")
+    Call<List<Post>> getListPostByUserId(@Query("userId") List<Integer> userId,
+                                         @Query("_sort") String sort,
+                                         @Query("_desc") String order);
+
+    @GET("posts")
+    Call<List<Post>> getMapPostByUserId(@QueryMap Map<String, String> parameters );
+
+    @GET
+    Call<List<Post>> getPostUrl(@Url String url);
 }
--- app/src/main/java/com/kabasonic/retrofitexample/MainActivity.java
+++ app/src/main/java/com/kabasonic/retrofitexample/MainActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -14,6 +17,8 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    private JsonApiPlaceHolder jsonApiPlaceHolder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,32 +30,227 @@ public class MainActivity extends AppCompatActivity {
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
-        JsonApiPlaceHolder jsonApiPlaceHolder = retrofit.create(JsonApiPlaceHolder.class);
+        jsonApiPlaceHolder = retrofit.create(JsonApiPlaceHolder.class);
 
-        Call<List<Post>> postsList = jsonApiPlaceHolder.getPosts();
+        // Get all posts /posts
+        //getAllPosts();
 
-        postsList.enqueue(new Callback<List<Post>>() {
+        //Get post by id /posts/1 (id)
+//        int postId = 3;
+//        getPostById(postId);
+
+//        Get posts/1/comments , where 1 it is postId
+//        int postId = 23;
+//        getCommentsForPost(postId);
+
+        //Get post by user Id sort by id, order desc
+//        int userId = 3;
+//        getPostByUserId(userId);
+
+        //Get list post by user id
+        //empty list get all records
+//        List<Integer> userId = new ArrayList<>();
+//        userId.add(1);
+//        userId.add(2);
+//        userId.add(3);
+//        getListPostByUserId(userId);
+
+        //Get list post by user id using map
+        //getMapPostByUserId();
+
+        //Get list post using url
+//        getPostUrl();
+
+    }
+
+    private void getPostUrl(){
+        Call<List<Post>> call = jsonApiPlaceHolder.getPostUrl("posts");
+        call.enqueue(new Callback<List<Post>>() {
             @Override
             public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
-                if(!response.isSuccessful()){
-                    Log.d(getClass().getSimpleName(),"onResponse|postList|Message" + response.message());
+                if (!response.isSuccessful()) {
+                    Log.d(getClass().getSimpleName(), "onResponse|postList|Message" + response.message());
                     return;
                 }
                 List<Post> callPosts = response.body();
-                for(Post post:callPosts){
-                    Log.i("#","##########");
-                    Log.i("#","--User ID: " + post.getUserId());
-                    Log.i("#","--ID: " + post.getId());
-                    Log.i("#","--Title: " + post.getTitle());
-                    Log.i("#","--Body: " + post.getText());
+                for (Post post : callPosts) {
+                    Log.i("#", "##########");
+                    Log.i("#", "--User ID: " + post.getUserId());
+                    Log.i("#", "--ID: " + post.getId());
+                    Log.i("#", "--Title: " + post.getTitle());
+                    Log.i("#", "--Body: " + post.getText());
                 }
             }
 
             @Override
             public void onFailure(Call<List<Post>> call, Throwable t) {
-                Log.d(getClass().getSimpleName(),"onFailure|postsList|Message: " + t.getMessage());
+                Log.d(getClass().getSimpleName(), "onFailure|postsList|Message: " + t.getMessage());
             }
         });
+    }
 
+    private void getMapPostByUserId(){
+        Map<String,String> queryMap = new HashMap<>();
+        queryMap.put("userId","1");
+        queryMap.put("_sort","id");
+        queryMap.put("_order","desc");
+        Call<List<Post>> call = jsonApiPlaceHolder.getMapPostByUserId(queryMap);
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                if (!response.isSuccessful()) {
+                    Log.d(getClass().getSimpleName(), "onResponse|postList|Message" + response.message());
+                    return;
+                }
+                List<Post> callPosts = response.body();
+                for (Post post : callPosts) {
+                    Log.i("#", "##########");
+                    Log.i("#", "--User ID: " + post.getUserId());
+                    Log.i("#", "--ID: " + post.getId());
+                    Log.i("#", "--Title: " + post.getTitle());
+                    Log.i("#", "--Body: " + post.getText());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                Log.d(getClass().getSimpleName(), "onFailure|postsList|Message: " + t.getMessage());
+            }
+        });
+    }
+
+    private void getListPostByUserId(List<Integer> userId){
+        Call<List<Post>> call = jsonApiPlaceHolder.getListPostByUserId(userId,"id","desc");
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                if (!response.isSuccessful()) {
+                    Log.d(getClass().getSimpleName(), "onResponse|postList|Message" + response.message());
+                    return;
+                }
+                List<Post> callPosts = response.body();
+                for (Post post : callPosts) {
+                    Log.i("#", "##########");
+                    Log.i("#", "--User ID: " + post.getUserId());
+                    Log.i("#", "--ID: " + post.getId());
+                    Log.i("#", "--Title: " + post.getTitle());
+                    Log.i("#", "--Body: " + post.getText());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                Log.d(getClass().getSimpleName(), "onFailure|postsList|Message: " + t.getMessage());
+            }
+        });
+    }
+
+    private void getPostByUserId(int userId){
+        Call<List<Post>> call = jsonApiPlaceHolder.getPostByUserId(userId,"id","desc");
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                if (!response.isSuccessful()) {
+                    Log.d(getClass().getSimpleName(), "onResponse|postList|Message" + response.message());
+                    return;
+                }
+                List<Post> callPosts = response.body();
+                for (Post post : callPosts) {
+                    Log.i("#", "##########");
+                    Log.i("#", "--User ID: " + post.getUserId());
+                    Log.i("#", "--ID: " + post.getId());
+                    Log.i("#", "--Title: " + post.getTitle());
+                    Log.i("#", "--Body: " + post.getText());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                Log.d(getClass().getSimpleName(), "onFailure|postsList|Message: " + t.getMessage());
+            }
+        });
+    }
+
+    private void getCommentsForPost(int postId) {
+        Call<List<Comment>> callCommentsList = jsonApiPlaceHolder.getCommentsForPost(postId);
+        callCommentsList.enqueue(new Callback<List<Comment>>() {
+            @Override
+            public void onResponse(Call<List<Comment>> call, Response<List<Comment>> response) {
+                if (!response.isSuccessful()) {
+                    Log.d("#", "onResponse|postList|Message" + response.message());
+                    return;
+                }
+                List<Comment> comments = response.body();
+                for (Comment comment : comments) {
+                    Log.i("#", "##########");
+                    Log.i("#", "--Post Id: " + comment.getPostId());
+                    Log.i("#", "--Id: " + comment.getId());
+                    Log.i("#", "--Name: " + comment.getName());
+                    Log.i("#", "--Email: " + comment.getEmail());
+                    Log.i("#", "--Text: " + comment.getText());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Comment>> call, Throwable t) {
+                Log.d("#", "onFailure|postsList|Message: " + t.getMessage());
+            }
+        });
+    }
+
+    private void getPostById(int postId) {
+        Call<List<Post>> call = jsonApiPlaceHolder.getPostById(postId);
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                if (!response.isSuccessful()) {
+                    Log.d("#", "onResponse|postList|Message" + response.message());
+                    return;
+                }
+                List<Post> callPosts = response.body();
+                if (callPosts.isEmpty()) {
+                    Log.d("#", "callPosts is empty");
+                }
+                for (Post post : callPosts) {
+                    Log.i("#", "##########");
+                    Log.i("#", "--User ID: " + post.getUserId());
+                    Log.i("#", "--ID: " + post.getId());
+                    Log.i("#", "--Title: " + post.getTitle());
+                    Log.i("#", "--Body: " + post.getText());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                Log.d("#", "onFailure|postsList|Message: " + t.getMessage());
+            }
+        });
+    }
+
+    private void getAllPosts() {
+        Call<List<Post>> postsList = jsonApiPlaceHolder.getPosts();
+
+        postsList.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                if (!response.isSuccessful()) {
+                    Log.d(getClass().getSimpleName(), "onResponse|postList|Message" + response.message());
+                    return;
+                }
+                List<Post> callPosts = response.body();
+                for (Post post : callPosts) {
+                    Log.i("#", "##########");
+                    Log.i("#", "--User ID: " + post.getUserId());
+                    Log.i("#", "--ID: " + post.getId());
+                    Log.i("#", "--Title: " + post.getTitle());
+                    Log.i("#", "--Body: " + post.getText());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                Log.d(getClass().getSimpleName(), "onFailure|postsList|Message: " + t.getMessage());
+            }
+        });
     }
 }
